87f3cd1cd411179c9ce49b1c71b1d935c99714a2
jyothsnakannati <jyoshnadevikannati@gmail.com>
2025-01-10 22:33:52 +0530

    first

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: 'https://github.com/jyothsnakannati/Quinx-SUDOKU/',  // Replace with your repository name
 });
+
